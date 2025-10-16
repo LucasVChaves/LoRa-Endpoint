8e4074e5b510bab3801c42a3114bfe6f85de84b7
Lucas <lucasvchaves@gmail.com>
2025-10-16 18:13:11 -0300

    pinagem do sensor em config

--- include/config.h
+++ include/config.h
@@ -17,7 +17,7 @@ const uint64_t SLEEP_INTERVAL_US = (uint64_t)SLEEP_INTERVAL_HOURS * 3600 * 10000
 
 // Tags para identificação dos logs nos diferentes módulos do sistema.
 #define TAG_MAIN "Main"
-#define TAG_FSM "AppFSM"
+#define TAG_FSM "FSM"
 #define TAG_LORA "Lora"
 #define TAG_SENSOR "SensorSvc"
 #define TAG_POWER "Power"
@@ -33,6 +33,10 @@ const uint64_t SLEEP_INTERVAL_US = (uint64_t)SLEEP_INTERVAL_HOURS * 3600 * 10000
 #define LORA_AUX_PIN 18
 #define LORA_M0_PIN 19
 #define LORA_M1_PIN 21
+
+// Pinos Sensores
+#define BMP280_SCL 22
+#define BMP280_SDA 23
 
 // LED de status
 #define LED_BUILT_IN 2
